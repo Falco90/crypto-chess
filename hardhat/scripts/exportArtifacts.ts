667fbf7435887261e6172cd38208bd18c87702e7
Falco90 <falcorodenburg@gmail.com>
2025-04-15 19:35:26 +0700

    add script to copy contract artifacts to server

---
+++ hardhat/scripts/exportArtifacts.ts
@@ -0,0 +1,35 @@
+// scripts/exportArtifacts.ts
+import fs from 'fs';
+import path from 'path';
+
+type ContractPath = {
+    source: string; // artifact location relative to project root
+    target?: string; // optional name to use in server/artifacts
+};
+
+// 💡 Add all contract paths (relative to artifacts/) here:
+const contractsToExport: ContractPath[] = [
+    { source: '@flarenetwork/flare-periphery-contracts/coston2/ContractRegistry.sol/ContractRegistry.json' },
+    { source: '@flarenetwork/flare-periphery-contracts/coston2/IJsonApiVerification.sol/IJsonApiVerification.json'},
+    { source: '@flarenetwork/flare-periphery-contracts/coston2/IFdcHub.sol/IFdcHub.json'},
+    { source: '@flarenetwork/flare-periphery-contracts/coston2/IFdcHub.sol/IFdcHub.json'},
+    { source: '@flarenetwork/flare-periphery-contracts/coston2/IFlareSystemsManager.sol/IFlareSystemsManager.json'},
+
+];
+
+const sourceBase = path.join(__dirname, '../artifacts');
+const targetBase = path.join(__dirname, '../../server/artifacts');
+
+if (!fs.existsSync(targetBase)) {
+    fs.mkdirSync(targetBase, { recursive: true });
+}
+
+for (const contract of contractsToExport) {
+    const { source, target } = contract;
+    const sourcePath = path.join(sourceBase, source);
+    const fileName = target || path.basename(source);
+    const targetPath = path.join(targetBase, fileName);
+
+    fs.copyFileSync(sourcePath, targetPath);
+    console.log(`✔ Copied ${source} → ${fileName}`);
+}
