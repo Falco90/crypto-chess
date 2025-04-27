07f999cd5ab1d495be1329540521d1d114ca0869
Falco90 <falcorodenburg@gmail.com>
2025-04-27 12:50:45 +0700

    removed unused chain, solve readContract error

--- webapp/src/wagmi.ts
+++ webapp/src/wagmi.ts
@@ -1,25 +1,14 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
-  arbitrum,
-  base,
   flareTestnet,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
     flareTestnet,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [flareTestnet] : []),
   ],
   ssr: true,
 });
